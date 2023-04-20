2743b3ece6af972551a9880e1efe280726a20563
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-04-20 23:52:01 +0700

    Outpu log file

---
+++ output_test.go
@@ -0,0 +1,18 @@
+package go_logging
+
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+	"testing"
+)
+
+func TestOutput(t *testing.T) {
+	logger := logrus.New()
+
+	file, _ := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	logger.SetOutput(file)
+
+	logger.Info("Hello Logging")
+	logger.Warning("Hello Logging")
+	logger.Error("Hello Logging")
+}
