d094f9c49a43e30d2392bb01d2597ec6f771ded5
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-04-23 20:07:26 +0700

    Loggrus singleton

---
+++ singleton_test.go
@@ -0,0 +1,16 @@
+package go_logging
+
+import (
+	"github.com/sirupsen/logrus"
+	"testing"
+)
+
+func TestSingleton(t *testing.T) {
+	logrus.Info("Hello World")
+	logrus.Warn("Hello World")
+
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	logrus.Info("Hello World")
+	logrus.Warn("Hello World")
+}
